8d7c26a3c577a3d7433a30b683d2b9f6a1e68e05
ja-carroll <james.carroll940@gmail.com>
2025-01-28 23:18:37 +0000

    added a basic login form

--- main.go
+++ main.go
@@ -13,8 +13,9 @@ type (
 )
 
 type model struct {
-	inputs []textinput.Model
-	err    error
+	inputs  []textinput.Model
+	focused int
+	err     error
 }
 
 func (m model) Init() tea.Cmd {
@@ -23,44 +24,98 @@ func (m model) Init() tea.Cmd {
 
 func (m model) View() string {
 	return fmt.Sprintf(
-		"Welcome to Easy-ftp Client!\n\nPlease enter the Hostname of the ftp server you wish to connect to\n\n%s\n\n%s",
+		`
+    Welcome to Easy-ftp Client!
+    Please enter the Hostname of the ftp server you wish to connect to
+
+    %s
+    %s
+    %s
+
+    %s`,
 		m.inputs[0].View(),
+		m.inputs[1].View(),
+		m.inputs[2].View(),
 		"(esc to quit)",
 	) + "\n"
 }
 
 func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
-	var cmd tea.Cmd
+	var cmds []tea.Cmd = make([]tea.Cmd, len(m.inputs))
 
 	switch msg := msg.(type) {
 	case tea.KeyMsg:
 		switch msg.Type {
-		case tea.KeyEnter, tea.KeyCtrlC, tea.KeyEsc:
+
+		case tea.KeyEnter:
+			if m.focused == len(m.inputs)-1 {
+				return m, tea.Quit
+			}
+			// Increment to the next input if we are not at the last input
+			m.nextInput()
+
+		case tea.KeyCtrlC, tea.KeyEsc:
 			return m, tea.Quit
+
+		case tea.KeyDown:
+			m.nextInput()
+		case tea.KeyUp:
+			m.prevInput()
 		}
+
+		for i := range m.inputs {
+			m.inputs[i].Blur()
+		}
+		m.inputs[m.focused].Focus()
 
 	case errMsg:
 		m.err = msg
 		return m, nil
 	}
 
-	m.inputs[0], cmd = m.inputs[0].Update(msg)
+	for i := range m.inputs {
+		m.inputs[i], cmds[i] = m.inputs[i].Update(msg)
+	}
 
-	return m, cmd
+	return m, tea.Batch(cmds...)
 }
 
-func main() {
-	ti := textinput.New()
-	ti.Placeholder = "Hostname"
-	ti.Focus()
-	ti.CharLimit = 200
-	ti.Width = 20
+func (m *model) nextInput() {
+	m.focused = (m.focused + 1) % len(m.inputs)
+}
 
-	mod := model{
-		inputs: []textinput.Model{ti},
-		err:    nil,
+func (m *model) prevInput() {
+	m.focused--
+
+	if m.focused < 0 {
+		m.focused = len(m.inputs) - 1
 	}
-	p := tea.NewProgram(mod)
+}
+
+func initialModel() model {
+	var inputs []textinput.Model = make([]textinput.Model, 3)
+	inputs[0] = textinput.New()
+	inputs[0].Placeholder = "Hostname"
+	inputs[0].CharLimit = 200
+	inputs[0].Prompt = ""
+	inputs[0].Focus()
+
+	inputs[1] = textinput.New()
+	inputs[1].Placeholder = "User"
+	inputs[1].Prompt = ""
+
+	inputs[2] = textinput.New()
+	inputs[2].Placeholder = "Pass"
+	inputs[2].Prompt = ""
+
+	return model{
+		inputs:  inputs,
+		focused: 0,
+		err:     nil,
+	}
+}
+func main() {
+	p := tea.NewProgram(initialModel())
 	if _, err := p.Run(); err != nil {
 		log.Fatal(err)
 	}
