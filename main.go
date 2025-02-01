8145fb9a78a9e2d20aee533ffd41bff767099686
ja-carroll <james.carroll940@gmail.com>
2025-02-01 00:05:04 +0000

    starting to use a huh form now

--- main.go
+++ main.go
@@ -1,11 +1,11 @@
 package main
 
 import (
-	"fmt"
 	"log"
 
 	"github.com/charmbracelet/bubbles/textinput"
 	tea "github.com/charmbracelet/bubbletea"
+	"github.com/charmbracelet/huh"
 )
 
 type (
@@ -16,65 +16,92 @@ type model struct {
 	inputs  []textinput.Model
 	focused int
 	err     error
+	form    *huh.Form // huh.Form is just a tea.Model
 }
 
 func (m model) Init() tea.Cmd {
-	return textinput.Blink
+	return m.form.Init()
 }
 
 func (m model) View() string {
-	return fmt.Sprintf(
-		`
-    Welcome to Easy-ftp Client!
-    Please enter the Hostname of the ftp server you wish to connect to
+	// return fmt.Sprintf(
+	//		`
+	//    Welcome to Easy-ftp Client!
+	//    Please enter the Hostname of the ftp server you wish to connect to
+	//
+	//    %s
+	//    %s
+	//    %s
+	//
+	//    %s`,
+	//		m.inputs[0].View(),
+	//		m.inputs[1].View(),
+	//		m.inputs[2].View(),
+	//		"(esc to quit)",
+	//	) + "\n"
 
-    %s
-    %s
-    %s
-
-    %s`,
-		m.inputs[0].View(),
-		m.inputs[1].View(),
-		m.inputs[2].View(),
-		"(esc to quit)",
-	) + "\n"
+	return m.form.View()
 }
 
 func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
-	var cmds []tea.Cmd = make([]tea.Cmd, len(m.inputs))
+	//	var cmds []tea.Cmd = make([]tea.Cmd, len(m.inputs))
+	//
+	//	switch msg := msg.(type) {
+	//	case tea.KeyMsg:
+	//		switch msg.Type {
+	//
+	//		case tea.KeyEnter:
+	//			if m.focused == len(m.inputs)-1 {
+	//				return m, tea.Quit
+	//			}
+	//			// Increment to the next input if we are not at the last input
+	//			m.nextInput()
+	//
+	//		case tea.KeyCtrlC, tea.KeyEsc:
+	//			return m, tea.Quit
+	//
+	//		case tea.KeyDown:
+	//			m.nextInput()
+	//		case tea.KeyUp:
+	//			m.prevInput()
+	//		}
+	//
+	//		for i := range m.inputs {
+	//			m.inputs[i].Blur()
+	//		}
+	//		m.inputs[m.focused].Focus()
+	//
+	//	case errMsg:
+	//		m.err = msg
+	//		return m, nil
+	//	}
+	//
+	//	for i := range m.inputs {
+	//		m.inputs[i], cmds[i] = m.inputs[i].Update(msg)
+	//	}
+	//
+	//	return m, tea.Batch(cmds...)
 
 	switch msg := msg.(type) {
 	case tea.KeyMsg:
-		switch msg.Type {
-
-		case tea.KeyEnter:
-			if m.focused == len(m.inputs)-1 {
-				return m, tea.Quit
-			}
-			// Increment to the next input if we are not at the last input
-			m.nextInput()
-
-		case tea.KeyCtrlC, tea.KeyEsc:
+		switch msg.String() {
+		case "ctrl+c":
 			return m, tea.Quit
-
-		case tea.KeyDown:
-			m.nextInput()
-		case tea.KeyUp:
-			m.prevInput()
+		case "esc", "q":
+			return m, tea.Quit
 		}
-
-		for i := range m.inputs {
-			m.inputs[i].Blur()
-		}
-		m.inputs[m.focused].Focus()
-
-	case errMsg:
-		m.err = msg
-		return m, nil
 	}
 
-	for i := range m.inputs {
-		m.inputs[i], cmds[i] = m.inputs[i].Update(msg)
+	var cmds []tea.Cmd
+
+	form, cmd := m.form.Update(msg)
+	if f, ok := form.(*huh.Form); ok {
+		m.form = f
+		cmds = append(cmds, cmd)
+	}
+
+	if m.form.State == huh.StateCompleted {
+		cmds = append(cmds, tea.Quit)
 	}
 
 	return m, tea.Batch(cmds...)
@@ -108,10 +135,19 @@ func initialModel() model {
 	inputs[2].Placeholder = "Pass"
 	inputs[2].Prompt = ""
 
+	form := huh.NewForm(
+		huh.NewGroup(
+			huh.NewInput().Title("Hostname").Key("host"),
+			huh.NewInput().Title("User").Key("key"),
+			huh.NewInput().Title("Password").Password(true).Key("pass"),
+		),
+	)
+
 	return model{
 		inputs:  inputs,
 		focused: 0,
 		err:     nil,
+		form:    form,
 	}
 }
 func main() {
